b7370ca6d9dbdd9f34f4f87c7c033bb182f89cae
Fuad <issafuad1818@gmail.com>
2025-06-17 14:47:36 -0700

    Publishing to render

--- Backend/index.js
+++ Backend/index.js
@@ -2,16 +2,46 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import userRoutes from './routes/userRoutes.js';
+import userRoutes from "./routes/userRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import expressStaticGzip from "express-static-gzip";
+
+//Resloving dirname for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Load environment variables
 dotenv.config();
 
 const app = express();
 
+// Routes
+app.use("/api/users", userRoutes);
+
 // Middleware
 app.use(cors()); // Allow frontend to access backend
 app.use(express.json()); // Parse JSON bodies
+
+// Use the Frontend app
+
+app.use(
+  "/",
+  expressStaticGzip(path.join(__dirname, "public"), {
+    enableBrotli: true,
+    orderPreference: ["br", "gz"],
+    setHeaders: function (res, path) {
+      res.setHeader("Cache-Control", "public, max-age=31536000");
+    },
+  })
+);
+
+app.use(express.static(path.join(__dirname, "../FrontEnd/dist")));
+
+// Catch-all handler to send back index.html for any other routes
+app.get('/{*any}', (req, res) => {
+  res.sendFile(path.join(__dirname, "../FrontEnd/dist/index.html"));
+});
 
 // MongoDB Connection
 mongoose
@@ -25,9 +55,6 @@ mongoose
   .catch((err) => {
     console.error("❌ MongoDB connection error:", err);
   });
-
-// Routes
-app.use("/api/users", userRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
