7ce818825cffba54250d05159f41c7e8e6f84e74
ChineduOscar <chinedunnadoziepatrick@gmail.com>
2025-10-01 17:00:18 +0100

    fixing the issues with csv images

--- FrontEnd/src/actions/convertGoogleDriveUrl.js
+++ FrontEnd/src/actions/convertGoogleDriveUrl.js
@@ -1,4 +1,4 @@
-const convertGoogleDriveUrl = (url) => {
+const convertGoogleDriveUrl = (url, size = 400) => {
   if (!url) return null;
 
   if (url.includes('drive.google.com')) {
@@ -10,10 +10,12 @@ const convertGoogleDriveUrl = (url) => {
       fileId = url.split('/file/d/')[1].split('/')[0];
     } else if (url.includes('uc?id=')) {
       fileId = url.split('uc?id=')[1].split('&')[0];
+    } else if (url.includes('thumbnail?id=')) {
+      fileId = url.split('thumbnail?id=')[1].split('&')[0];
     }
 
     if (fileId) {
-      return `https://drive.google.com/uc?export=view&id=${fileId}`;
+      return `https://drive.google.com/thumbnail?id=${fileId}&sz=w${size}`;
     }
   }
 
